c33027227315c4a3908cad25ef430242b95b0a49
Achieve-software <vtcbcdtyr@gmail.com>
2023-01-19 04:21:52 +0300

    for--sum--

--- sum.js
+++ sum.js
@@ -1,6 +1,6 @@
 //1 den- kullanıcıdan aldığın n sayısına kadar sayıların toplamını göster
 
-let sayi = prompt("kaça kadar sayı toplansın");
+let sayi = +prompt("kaça kadar sayı toplansın");
 let sum = 0;
 for (let i = 0; i <= sayi; i++) {
   sum = sum + i;
